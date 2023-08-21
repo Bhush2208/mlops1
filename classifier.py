5604ae88d629eae2096bc83557f1df0bd97ab4eb
Bhush2208 <kamanebhushan@gmail.com>
2023-08-21 08:28:50 +0000

    first commit

--- classifier.py
+++ classifier.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 df = pd.read_csv('/content/mlops1/data/Iris.csv')
 features = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 target = ['Species']
-X_train,X_test,y_train,y_test = train_test_split(df[features],df[target],test_size=0.1,shuffle=True)
+X_train,X_test,y_train,y_test = train_test_split(df[features],df[target],test_size=0.20,shuffle=True)
 
 clf = DecisionTreeClassifier(criterion = 'entropy')
 clf.fit(X_train,y_train)
